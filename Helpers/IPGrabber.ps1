4be3c7c2f821a393b7613b63fcd5b15376556365
Lucas Camilo <lucas@camilo.com.ar>
2016-09-09 17:49:28 -0300

    added ipgrabber

---
+++ Helpers/IPGrabber.ps1
@@ -0,0 +1,24 @@
+function get-computerip ($targetcomputer)
+{
+	$returnthis = New-Object –TypeName PSObject
+	$returnthis | Add-Member –MemberType NoteProperty –Name Name -Value $targetcomputer
+	$returnthis | Add-Member –MemberType NoteProperty –Name IPv4Address -Value ""
+	$returnthis | Add-Member –MemberType NoteProperty –Name IPv6Address -Value ""
+	$returnthis | Add-Member –MemberType NoteProperty –Name Online -Value $false
+	
+	$help=test-connection $targetcomputer -count 1 2>null
+	if ($help){
+		$returnthis.IPv4Address=$help.IPv4Address
+		$returnthis.IPv6Address=$help.ProtocolAddress
+		$returnthis.Online=$true
+	} 
+	
+	return $returnthis
+}
+
+$listafinal=@()
+
+foreach ($server in $serverlist){
+	$name=$server.name
+	$listafinal+=get-computerip $name
+}
